3705f47e4bc919488dd10a5896d720341a41578d
jetal <jetal@DESKTOP-FM7ARA9>
2019-03-15 21:14:02 -0400

    changed page title

--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Book an appointment</title>
+	<title>FAQ</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
