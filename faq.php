234a54b702203762977cabb3366394bfeb4be431
Ekta <ektapatel71292@gmail.com>
2019-03-19 09:34:18 -0400

    Ekta works

---
+++ faq.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FAQ</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel='stylesheet' type='text/css' href='styles/style.css'>
+  <link rel='stylesheet' type='text/css' href='styles/faq.css'>
+</head>
+
+<body>
+<!-- header.php -->
+
+	<?php require_once 'body/header.php' ?>
+<main id="faq-main">
+    <div><img src="images/faq_icon.png" alt="faq icon image"></div>
+    <div id="que-ans">
+     <p>Questions will come from database.</p>
+     <p>Answers will come from database.</p>
+    
+    
+    
+    </div>
+</main>
+    
+<footer>
+	<?php require_once 'body/footer.php' ?>
+</footer>
+</body>
+</html>
