bc765ff3f50768bd9c55868d24f85f891d956af8
Margi Patel <margi24patel@gmail.com>
2019-03-22 20:27:34 -0400

    margi added career feature with her database

--- addemployee_client_index.php
+++ addemployee_client_index.php
@@ -1,6 +1,6 @@
+
 <!--Add Employee Client Side(Employee will applay for a job ) -->
 <?php
-
 
 echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
         <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js'></script>";
@@ -8,7 +8,7 @@ echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.mi
 echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css'>";
 
 echo "<!-- Font Awesome -->
-          <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>";
+      <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>";
      //Margi - Custom files
 echo "<link rel='stylesheet' type='text/css' href='styles/style.css'>";
 echo "<link rel='stylesheet' href='styles/addemployee_client_index.css'>";
@@ -22,83 +22,325 @@ echo "<link rel='stylesheet' href='styles/addemployee_client_index.css'>";
 <!-- main -->
 <main id="addemployee_client_index_main">
 <div class="container">
-    <h1>Employment Form</h1>
+    <h1>Employement Form</h1>
+    
+<?php  
+    // added for foreign key this 'id' comes  name = 'id' , name='apply' from <div id="applay-job> careere_main_page. 
+    //this post_id is for when employee will apply for particular position. 
+    $post_id = "";
+    if(isset($_POST['apply'] )){
+        $post_id = $_POST['id'];
+    }
+?>
+<?php
+    require_once 'database/career/DatabaseEmployeesCareer.php';
+    require_once 'database/career/JobPosition_admin.php';
+    require_once 'database/career/Employee_client.php';
 
-    
-<div class="container">
-<form action=""  class="form-horizontal">
-    
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="employee_f_name"> Employee First Name:</label>
-        <div class="col-sm-6">
-            <input type="textbox" name="employee_f_name" class="form-control" />
-        </div>
-    </div>
+    $response_msg=""; //response message for employee
 
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="employee_l_name"> Employee Last Name:</label>
-        <div class="col-sm-6">
-            <input type="textbox" name="employee_l_name" class="form-control"/>
-        </div>
-    </div>
-    
-    <div class="form-group">
-       <label class="control-label col-sm-2" for="streetname">Street Name:</label>
-       <div class="col-sm-6">
-            <input type="text" name="streetname" class="form-control" />
-       </div>
-    </div>
+    if(isset($_POST['addemp']))
+    {
+       $job_id =$_POST['job_id'];
+       $employee_firstname = $_POST['employee_firstname'];
+       $employee_lastname = $_POST['employee_lastname'];
+       $streetname = $_POST['streetname'];
+       $city = $_POST['city'];
+       $province = $_POST['province'];
+       $postal_code = $_POST['postal_code'];
+       $email_id = $_POST['email_id'];
+       $phone_no = $_POST['phone_no'];
+       $availability = $_POST['availability'];
+       
+    //VALIDATION
      
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="city">City:</label>
-      <div class="col-sm-6">
-            <input type="text" name="city"  class="form-control" />
-      </div>
-    </div>
-     
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="province">Province:</label>
-      <div class="col-sm-6">  
-            <input type="text" name="province"  class="form-control" />
+        function text_validation($value)
+        {
+            if($value == "")
+            {
+                return "Please enter value";
+            }
+            else
+            {
+                return "";
+            }
+        }
+        
+    //function for First Name, Last Name, Postal Code, Phone No., Email Id
+   
+        function validate($value, $pattern = "")
+        {
+            if($value == "")
+                {
+                    return "Please enter value";
+                }
+            elseif(!preg_match($pattern, $value))
+                {
+                    return "Please enter value properly";
+                }
+            else
+                {
+                    return "";
+                }
+       }
+        //for First Name
+        $patternfname = "/^[a-zA-Z ]{2,30}$/";
+        $fname_err = validate($employee_firstname, $patternfname);    
+        
+        //for Last Name
+        $patternlname = "/^[a-zA-Z ]{2,30}$/";
+        $lname_err = validate($employee_lastname, $patternlname);   
+        
+        //for Street Name
+        $streetname_err = text_validation($streetname);
+        
+        //for City 
+        $city_err = text_validation($city);
+        
+        //for Province
+        $province_err = text_validation($province);
+        
+        
+        //for Postal Code
+        $patternPC = "/\w\d\w\s\d\w\d/";
+        $pc_err = validate($postal_code,$patternPC);
+       
+        //for Phone No.
+        $patternPhNo = "/\d{10}/"; 
+        $ph_err = validate($phone_no,$patternPhNo);
+        
+        //for Email Id
+        $patternEmail = "/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/"; 
+        $email_err = validate($email_id,$patternEmail);
+        
+        //availablity
+        $availability_err = text_validation($availability);
+        
+        
+        
+    //Connection with database
+       if(!empty($_POST['employee_firstname']) && !empty($_POST['employee_lastname']) && !empty($_POST['streetname']) && !empty($_POST['city']) && !empty($_POST['province']) && !empty($_POST['postal_code']) && !empty($_POST['email_id']) && !empty($_POST['phone_no']) && !empty($_POST['availability']))
+       {
+            $dbcon = DatabaseEmployeesCareer::getDb();
+            $e = new Employee_client();
+            $c = $e->addEmployee($job_id, $employee_firstname, $employee_lastname, $streetname, $city, $province, $postal_code, $email_id, $phone_no, $availability, $dbcon);
+            
+            
+            if($c)
+            {
+                 $response_msg = "Thank You  $employee_firstname $employee_lastname for your Interest! We will response you soon on  $email_id";
+            } 
+           else 
+            {
+                echo "problem adding a employee";
+            } 
+          
+        }
+        else
+        {
+            echo "Please Fill All Required Fields";
+            
+        }
+       
+      }    
+    
+?>
+
+    <form action="" method="post" class="form-horizontal">
+        <!-- job id-->
+        <div class="form-group">
+            <input type="hidden" name="job_id" value="<?= $post_id; ?>" /> 
         </div>
-    </div>
-    
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="postal_code">Postal Code:</label>
-        <div class="col-sm-6">
-            <input type="text" name="postal_code" class="form-control" />
+
+        <!--Employee First Name -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="employee_firstname"> Employee First Name:</label>
+            <div class="col-sm-6">
+            <input type="textbox" name="employee_firstname" class="form-control" value="<?php
+                                if(isset($employee_firstname)){
+                                    echo $employee_firstname;
+                                }
+                          ?>" class="form-control"/>
+             </div>   
+                <span style="color:red;">
+                <?php
+                    if(isset($fname_err)){
+                        echo $fname_err;
+                    }
+                    ?>
+                </span><br /> 
+        </div><br><br>
+
+        <!--Employee Last Name -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="employee_lastname"> Employee Last Name:</label>
+            <div class="col-sm-6">
+            <input type="textbox" name="employee_lastname" class="form-control" value="<?php
+                                if(isset($employee_lastname)){
+                                    echo $employee_lastname;
+                                }
+                          ?>" class="form-control"/>
+            </div>   
+                <span style="color:red;">
+                <?php
+                    if(isset($lname_err)){
+                        echo $lname_err;
+                    }
+                    ?>
+                </span><br /> 
+        </div><br><br>
+
+        <!--Street Name -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="streetname">Street Name:</label>
+          <div class="col-sm-6">    
+          <input type="text" name="streetname" value="<?php
+                                if(isset($streetname)){
+                                        echo $streetname;
+                                }
+                          ?>" class="form-control"/>
+          </div>      
+                <span style="color:red;">
+                <?php 
+                    if(isset($streetname_err)){
+                        echo $streetname_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--City -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="city">City:</label>
+          <div class="col-sm-6">        
+          <input type="text" name="city"  class="form-control" value="<?php
+                                if(isset($city)){
+                                        echo $city;
+                                }
+                          ?>" class="form-control" />
+          </div>    
+                <span style="color:red;">
+                <?php 
+                    if(isset($city_err)){
+                        echo $city_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--Province -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="province">Province:</label>
+          <div class="col-sm-6">            
+          <input type="text" name="province" class="form-control" value="<?php
+                                if(isset($province)){
+                                        echo $province;
+                                }
+                          ?>" class="form-control" />
+            </div>      
+                <span style="color:red;">
+                <?php 
+                    if(isset($province_err)){
+                        echo $province_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--Postal Code -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="postal_code">Postal Code:</label>
+          <div class="col-sm-6">               
+          <input type="text" name="postal_code" class="form-control" value="<?php
+                                if(isset($postal_code)){
+                                        echo $postal_code;
+                                }
+                          ?>" class="form-control" />&nbsp;&nbsp;(Canada Only)&nbsp;(E.G. M4B 8J6)
+          </div>        
+                <span style="color:red;">
+                <?php 
+                    if(isset($pc_err)){
+                        echo $pc_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--Email Id -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="email_id">Email Id:</label>
+          <div class="col-sm-6">       
+          <input type="text" name="email_id" class="form-control" value="<?php
+                                if(isset($email_id)){
+                                    echo $email_id;
+                }
+                          ?>" class="form-control"/>&nbsp;&nbsp;(e.g. me@example.com)
+            </div>     
+                <span style="color:red;">
+                <?php 
+                    if(isset($email_err)){
+                        echo $email_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--Phone No. -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="phone_no">Phone No:</label>
+          <div class="col-sm-6">      
+          <input type="text" name="phone_no" class="form-control" value="<?php
+                                if(isset($phone_no)){
+                                    echo $phone_no;
+                                }
+                          ?>" class="form-control" />&nbsp;&nbsp;(e.g. 4171232757)
+          </div>      
+                <span style="color:red;">
+                <?php
+                    if(isset($ph_err)){
+                        echo $ph_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+        <!--Availibility -->
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="availability">Availibility:</label>
+          <div class="col-sm-6">     
+          <input type="date" name="availability" class="form-control" value="<?php
+                                if(isset($availability)){
+                                        echo $availability;
+                                }
+                          ?>" class="form-control" />
+            </div>     
+                <span style="color:red;">
+                <?php 
+                    if(isset($availability_err)){
+                        echo $availability_err;
+                    }
+                    ?>
+                </span><br />
+        </div><br><br>
+
+       <br>
+
+        <!--Submit Button -->
+       <div class="form-group">        
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default" name="addemp" value="Submit">Submit</button>
+          </div>
         </div>
-    </div>
-    
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="email_id">Email Id:</label>
-        <div class="col-sm-6">
-        <input type="text" name="email_id" class="form-control" />
-        </div>
-    </div>
-    
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="phone_no">Phone No:</label>
-        <div class="col-sm-6">
-        <input type="text" name="phone_no" class="form-control" />
-        </div>
-    </div>
-    
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="availability">Availibility:</label>
-        <div class="col-sm-6">
-        <input type="text" name="availability" class="form-control" />
-        </div>
-    </div>
-    
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-    
-</form>
-</div>
+
+     <!--   <input type="submit" name="addemp" value="Submit" class="btn btn-default"> -->
+    </form>
+    <!-- Response Message -->
+   <div id="response_message">
+       <?php
+           echo $response_msg;
+       ?>
+    </div> 
+  
 </div>
 </main>
 
--- editjobposition_admin.php
+++ editjobposition_admin.php
@@ -9,7 +9,7 @@ echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.mi
 echo "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css'>";
 
 echo "<!-- Font Awesome -->
-          <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>";
+     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>";
      //Margi - Custom files
 echo "<link rel='stylesheet' type='text/css' href='styles/style.css'>";
 echo "<link rel='stylesheet' href='styles/editjobposition_admin.css'>";
@@ -22,81 +22,147 @@ echo "<link rel='stylesheet' href='styles/editjobposition_admin.css'>";
 
 <!-- main -->
 <main id="editjobposition_admin_main">
-<div class="container">
+ <div class="container">
     <h1>Edit Job Post</h1>
 
-    
-<div class="container">
-<form action=""  class="form-horizontal">
-    
 
-    
+<?php
+
+    require_once 'database/career/DatabaseEmployeesCareer.php';
+    require_once 'database/career/JobPosition_admin.php';
+
+    $job_title = $location_name = $skill_requirements = $job_requirements = $description = $salary = $job_type = "";
+
+
+    if(isset($_POST['id']))
+    {
+        $id = $_POST['id'];
+        $dbcon = DatabaseEmployeesCareer::getDb();
+
+        $sql = "SELECT * FROM job_post_admin_side where id = :id";
+        $pst = $dbcon->prepare($sql);
+        $pst->bindParam(':id', $id);
+        $pst->execute();
+
+        $position = $pst->fetch(PDO::FETCH_OBJ);
+
+        $job_title =  $position->job_title;
+        $location_name = $position->location_name;
+        $skill_requirements = $position->skill_requirements;
+        $job_requirements = $position->job_requirements;
+        $description = $position->description;
+        $salary = $position->salary;
+        $job_type = $position->job_type;
+
+
+    }
+    if(isset($_POST['editjobposition']))
+    {
+        $id = $_POST['pid'];
+        $job_title = $_POST['job_title'];
+        $location_name = $_POST['location_name'];
+        $skill_requirements = $_POST['skill_requirements'];
+        $job_requirements = $_POST['job_requirements'];
+        $description = $_POST['description'];
+        $salary = $_POST['salary'];
+        $job_type = $_POST['job_type'];
+
+        $dbcon = DatabaseEmployeesCareer::getDb();
+        $j = new JobPosition_admin();
+
+        $count = $j->updateJobPosition($id, $job_title, $location_name, $skill_requirements, $job_requirements, $description, $salary, $job_type, $dbcon);
+
+        if($count){
+           header('Location:  listjobpositions_admin.php');
+            //echo "updated succesfully";
+        } else {
+            echo "problem";
+        }
+    }
+  
+
+?>
+ 
+<!-- form -->
+    <form action="" method="post" class="form-horizontal">
+
+        <!-- id-->
         <div class="form-group">
-        <label class="control-label col-sm-2" for="job_title"></label>
-        <div class="col-sm-6">
-            <input type="hidden" name="pid" value="<?= $id; ?>" class="form-control"/>  
+            <label class="control-label col-sm-2" for="job_title"></label>
+                <div class="col-sm-6">
+                    <input type="hidden" name="pid" value="<?= $id; ?>" class="form-control"/>
+                </div>
         </div>
-    </div>
-    
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="job_title"> Job Title:</label>
-        <div class="col-sm-6">
-            <input type="textbox" name="job_title" class="form-control" />
-        </div>
-    </div>
 
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="location_name"> Location Name:</label>
-        <div class="col-sm-6">
-            <input type="textbox" name="location_name" class="form-control"/>
+        <!-- Job Title -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="job_title"> Job Title:</label>
+                <div class="col-sm-6">
+                    <input type="text" name="job_title" value="<?= $job_title; ?>" class="form-control"/>
+            </div>
         </div>
-    </div>
-     
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="skill_requirements">Skill Requirements:</label>
-      <div class="col-sm-6">
-            <input type="text" name="skill_requirements"  class="form-control" />
-      </div>
-    </div>
-     
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="job_requirements">Job Requirements:</label>
-      <div class="col-sm-6">  
-            <input type="text" name="job_requirements"  class="form-control" />
-        </div>
-    </div>
-    
-    <div class="form-group">
-       <label class="control-label col-sm-2" for="description">Description:</label>
-       <div class="col-sm-6">
-            <input type="text" name="description" class="form-control" />
-       </div>
-    </div>
-    
-    <div class="form-group">
-       <label class="control-label col-sm-2" for="salary">Salary:</label>
-       <div class="col-sm-6">
-            <input type="text" name="salary" class="form-control" />
-       </div>
-    </div>
-    
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="job_type">Job Type:</label>
-        <div class="col-sm-6">
-            <input type="text" name="job_type" class="form-control" />
-        </div>
-    </div>
 
-    
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-    
-</form>
-</div>
-</div>
+        <!-- Location Name -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="location_name"> Location Name:</label>
+                <div class="col-sm-6">
+                    <input type="text" name="location_name" value="<?= $location_name; ?>" class="form-control"/>
+            </div>
+        </div>
+
+
+        <!-- Skill Requirements -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="skill_requirements"> Skill Requirements:</label>
+                <div class="col-sm-6">
+                    <input type="text" name="skill_requirements" value="<?= $skill_requirements; ?>" class="form-control"/>
+            </div>
+        </div>
+
+
+        <!-- Job Requirements-->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="job_requirements"> Job Requirements:</label>
+               <div class="col-sm-6"> 
+                   <input type="text" name="job_requirements" value="<?= $job_requirements; ?>" class="form-control"/>
+            </div>
+        </div>
+
+        <!-- Description -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="description"> Description: </label>
+                 <div class="col-sm-6"> 
+                    <input type="text" name="description" value="<?= $description; ?>" class="form-control"/>
+            </div>
+        </div>
+
+        <!-- Salary -->
+        <div class="form-group">
+             <label class="control-label col-sm-2" for="salary"> Salary:  </label>
+                <div class="col-sm-6"> 
+                    <input type="text" name="salary" value="<?= $salary ?>" class="form-control"/>
+            </div>
+        </div>
+
+        <!-- Job Type -->
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="job_type"> Job Type:  </label>
+                <div class="col-sm-6">
+                    <input type="text" name="job_type" value="<?= $job_type ?>" class="form-control"/>
+            </div>
+        </div>
+
+
+        <!-- Edit Button-->
+        <div class="form-group">        
+          <div class="col-sm-offset-2 col-sm-10">
+              <button type="submit" class="btn btn-default" name="editjobposition" value="Edit Job Post">Submit</button>
+            </div>
+        </div>
+
+      <!--  <input type="submit" name="editjobposition" value="Edit Job Post"> -->
+    </form>
+ </div>
 </main>
 
 <!-- footer -->
